04fa9771fcd07674d2bebfad042f03cf02c9e875
chenzh <lonely829@gmail.com>
2023-01-07 18:52:48 +0800

    Javis

--- app.py
+++ app.py
@@ -22,14 +22,21 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
+#
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(animal):
+    return """
+    Human: Hello, who are you?
+    Javis: I am an AI created by OpenAI. How can I help you today?
+    Human: {}
+    """.format(animal)
